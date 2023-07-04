fcd72a468d8dcffc1db829fcd3aa665fe95d8411
Andres Cuevas <testo_acg@hotmail.com>
2023-07-04 12:41:05 -0600

    ftr:Agregando logos de patrocinadores

--- src/components/brands/Brands.jsx
+++ src/components/brands/Brands.jsx
@@ -1,4 +1,11 @@
-import BRAND from "../../assets/imgs/image.png";
+import BRAND0 from "../../assets/imgs/brand0.png";
+import BRAND1 from "../../assets/imgs/brand1.png";
+import BRAND2 from "../../assets/imgs/brand2.png";
+import BRAND3 from "../../assets/imgs/brand3.png";
+import BRAND4 from "../../assets/imgs/brand4.png";
+import BRAND5 from "../../assets/imgs/brand5.png";
+import BRAND6 from "../../assets/imgs/brand6.png";
+import BRAND7 from "../../assets/imgs/brand7.png";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
@@ -39,61 +46,54 @@ const Brands = () => {
           className="mySwiper"
         >
           <SwiperSlide>
-            <img src={BRAND} alt="logotipo de marca patrocinadora" />
+            <img src={BRAND0} alt="logotipo de marca patrocinadora" />
           </SwiperSlide>
           <SwiperSlide>
             <img
               className="img__brand"
-              src={BRAND}
+              src={BRAND1}
               alt="logotipo de marca patrocinadora"
             />
           </SwiperSlide>
           <SwiperSlide>
             <img
               className="img__brand"
-              src={BRAND}
+              src={BRAND2}
               alt="logotipo de marca patrocinadora"
             />
           </SwiperSlide>
           <SwiperSlide>
             <img
               className="img__brand"
-              src={BRAND}
+              src={BRAND3}
               alt="logotipo de marca patrocinadora"
             />
           </SwiperSlide>
           <SwiperSlide>
             <img
               className="img__brand"
-              src={BRAND}
+              src={BRAND4}
               alt="logotipo de marca patrocinadora"
             />
           </SwiperSlide>
           <SwiperSlide>
             <img
               className="img__brand"
-              src={BRAND}
+              src={BRAND5}
               alt="logotipo de marca patrocinadora"
             />
           </SwiperSlide>
           <SwiperSlide>
             <img
               className="img__brand"
-              src={BRAND}
+              src={BRAND6}
               alt="logotipo de marca patrocinadora"
             />
           </SwiperSlide>
           <SwiperSlide>
             <img
               className="img__brand"
-              src={BRAND}
-              alt="logotipo de marca patrocinadora"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              className="img__brand"
-              src={BRAND}
+              src={BRAND7}
               alt="logotipo de marca patrocinadora"
             />
           </SwiperSlide>
